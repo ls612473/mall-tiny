9bc59e17aa30df73c58012fccf7d226af2e42591
shun.li <shun.li@qizhidao.cn>
2020-09-14 14:06:54 +0800

    初始化数据库密码 admin/admin test/123456

--- src/test/java/com/macro/mall/tiny/MallTinyApplicationTests.java
+++ src/test/java/com/macro/mall/tiny/MallTinyApplicationTests.java
@@ -2,15 +2,24 @@ package com.macro.mall.tiny;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallTinyApplicationTests {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     public void contextLoads() {
+        String value = passwordEncoder.encode("admin");
+        String value1 = passwordEncoder.encode("123456");
+        System.out.println(value);
+        System.out.println(value1);
     }
 
 }
